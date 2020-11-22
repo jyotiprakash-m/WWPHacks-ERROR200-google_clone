29909ccf4009f85b027e9c5b2a0e0bedbd4ff875
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-11-22 10:45:18 +0530

    Built Home Page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import React from 'react';
 import './App.css';
-import Home from "./Home"
-
+import Home from './pages/Home';
+import {BrowserRouter as Router,Switch,Route} from 
+'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-        <h2>Let`s build Google-Clone</h2>
+      <Router>
+        <Switch>
 
-    {/* Home page */}
+        <Route path="/search">
+            {/* Search Page */}
+          </Route>
 
-    <Home />
+          <Route path="/">
+            {/* Home Page */}
+            <Home />
+          </Route>
 
-    {/* Result page */}
-
-
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Home.css"
+import {Link} from "react-router-dom";
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';
+
+function Home() {
+    return (
+        <div className="home">
+            {/* BEM Convention */}
+            <div className='home__header'>
+                <div className="home__headerLeft">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                </div>
+            </div>
+            <div className="home__body">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
