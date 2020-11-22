f726c2457ecf627809e01db7d309d5818626815e
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-11-22 17:39:34 +0530

    Add functionality to searchPage

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY="Your Key";
+
+export default API_KEY;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,7 +9,7 @@ import { useHistory } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import {actionTypes} from "../reducer";
 
-function Search({hideButtons=false}) {
+function Search({ hideButtons=false }) {
 
     const [{},dispatch] =useStateValue();
 
